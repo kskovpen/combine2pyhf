7ac684751ec130d44201d3ff0f84787c29ec0fd3
kskovpen <Kirill.Skovpen@cern.ch>
2023-05-20 13:09:24 +0200

    Update test.sh

--- validation/test.sh
+++ validation/test.sh
@@ -7,7 +7,6 @@ mount -t cvmfs sft.cern.ch /cvmfs/sft.cern.ch
 cd /HiggsAnalysis/CombinedLimit
 . env_lcg.sh
 echo "Ready. Starting combine tests .."
-cp validation/combine.py .
 python3 combine.py
 echo "Done."
 
