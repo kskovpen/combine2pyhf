eee87452953db7825450351ca1e3d6f529f8ca2f
Kirill Skovpen <kskovpen@cern.ch>
2023-05-23 09:31:36 +0200

    update

--- converter/pyhf2combine.py
+++ converter/pyhf2combine.py
@@ -87,16 +87,16 @@ if __name__ == '__main__':
         if s == poi: continue
         sampord.append(s)
     nsamp = len(sampord)
-    dc = 'imax '+str(nchan)+' number of bins\\n'
-    dc += 'jmax '+str(nsamp-1)+' number of processes minus 1\\n'
-    dc += 'kmax 0 number of nuisance parameters\\n'
-    dc += '------------------------------------\\n'
+    dc = 'imax '+str(nchan)+' number of bins\n'
+    dc += 'jmax '+str(nsamp-1)+' number of processes minus 1\n'
+    dc += 'kmax 0 number of nuisance parameters\n'
+    dc += '------------------------------------\n'
     for ch in chans:
-        dc += 'shapes * '+ch+' '+options.output.split('/')[-1]+'.root $PROCESS\\n'
-    dc += '------------------------------------\\n'
-    dc += 'bin          '+' '.join(chans)+'\\n'
-    dc += 'observation  '+' '.join(np.repeat('-1 ', nchan))+'\\n'
-    dc += '------------------------------------\\n'
+        dc += 'shapes * '+ch+' '+options.output.split('/')[-1]+'.root $PROCESS\n'
+    dc += '------------------------------------\n'
+    dc += 'bin          '+' '.join(chans)+'\n'
+    dc += 'observation  '+' '.join(np.repeat('-1 ', nchan))+'\n'
+    dc += '------------------------------------\n'
     procbin, procname, procsamp, rate = [], [], [], []
     for ch in d['channels']:
         for i, s in enumerate(sampord):
@@ -104,13 +104,13 @@ if __name__ == '__main__':
             procname.append(s)
             procsamp.append(str(i))
             rate.append(str(-1))
-    dc += 'bin          '+' '.join(procbin)+'\\n'
-    dc += 'process      '+' '.join(procname)+'\\n'
-    dc += 'process      '+' '.join(procsamp)+'\\n'
-    dc += 'rate         '+' '.join(rate)+'\\n'
-    dc += '------------------------------------\\n'
-    if bbl: dc += 'ch1 autoMCStats 0 1 1\\n'
-    else: dc += 'ch1 autoMCStats 0 100000 100000\\n'
+    dc += 'bin          '+' '.join(procbin)+'\n'
+    dc += 'process      '+' '.join(procname)+'\n'
+    dc += 'process      '+' '.join(procsamp)+'\n'
+    dc += 'rate         '+' '.join(rate)+'\n'
+    dc += '------------------------------------\n'
+    if bbl: dc += 'ch1 autoMCStats 0 1 1\n'
+    else: dc += 'ch1 autoMCStats 0 100000 100000\n'
     print('print out internal')
     print(dc)
     with open(options.output+'.txt', 'w') as f:
