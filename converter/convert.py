99ad73263b9c082942ea7463eb65e3765fab7b20
Kirill Skovpen <kskovpen@cern.ch>
2023-06-10 13:41:40 +0200

    update

--- converter/convert.py
+++ converter/convert.py
@@ -9,6 +9,7 @@ wd = ws+'/validation'
 dc = glob.glob(ws+'/cards/combine/*')
 
 def shapeloc(dname, fname):
+    print('modify '+fname)
     with open(fname+'_mod', 'w') as f:
         with open(fname, 'r') as fr:
             for line in fr:
@@ -16,7 +17,9 @@ def shapeloc(dname, fname):
                     words = line.split()
                     for i, w in enumerate(words):
                         if '.root' in w:
+                            print('old: '+' '.join(words))
                             words[i] = wd+'/cards/'+dname+'/'+w
+                            print('new: '+' '.join(words))
                     f.write(' '.join(words)+'\n')
                 else: f.write(line)
     os.system('mv '+fname+'_mod '+fname)
@@ -44,13 +47,10 @@ for d in dc:
     os.system('mkdir -p '+wd+'/cards/pyhf/pyhf2combine/'+dname)
     os.system('mkdir -p '+wd+'/cards/pyhf/combine2pyhf/'+dname)
     fc = glob.glob(ws+'/cards/pyhf/'+dname+'/*.json')
-    print('testing')
-    fl = glob.glob('/__w/combine2pyhf/validation/cards/pyhf/pyhf2combine/one-bin/*')
-    for f in fl:
-        print(f)
     for f in fc:
         fname = f.split('/')[-1]
         print('pyhf -> combine: '+fname)
+        print('output: '+wd+'/cards/pyhf/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0])
         os.system('python3 '+ws+'/converter/pyhf2combine.py --input '+f+' --output '+wd+'/cards/pyhf/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0])
         print('combine -> pyhf: '+fname)
         shapeloc('pyhf/pyhf2combine/'+dname, wd+'/cards/pyhf/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0]+'.txt')
