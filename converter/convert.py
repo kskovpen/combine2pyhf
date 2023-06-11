d45295363187a83318d9a7dd03f9e2d77906ecce
Kirill Skovpen <kskovpen@cern.ch>
2023-06-11 15:13:15 +0200

    update

--- converter/convert.py
+++ converter/convert.py
@@ -8,8 +8,7 @@ wd = ws+'/validation'
 # combine cards
 dc = glob.glob(ws+'/cards/combine/*')
 
-def shapeloc(dname, fname, combine2pyhf = False):
-    print('modify '+fname)
+def shapeloc(dname, fname):
     with open(fname+'_mod', 'w') as f:
         with open(fname, 'r') as fr:
             for line in fr:
@@ -17,8 +16,7 @@ def shapeloc(dname, fname, combine2pyhf = False):
                     words = line.split()
                     for i, w in enumerate(words):
                         if '.root' in w:
-                            if combine2pyhf: words[i] = ws+'/cards/combine/'+dname+'/'+w
-                            else: words[i] = wd+'/cards/'+dname+'/'+w
+                            words[i] = wd+'/cards/'+dname+'/'+w
                     f.write(' '.join(words)+'\n')
                 else: f.write(line)
     os.system('mv '+fname+'_mod '+fname)
@@ -28,10 +26,11 @@ for d in dc:
     print('Convert '+dname+' (combine)')
     os.system('mkdir -p '+wd+'/cards/combine/combine2pyhf/'+dname)
     os.system('mkdir -p '+wd+'/cards/combine/pyhf2combine/'+dname)
-    fc = glob.glob(ws+'/cards/combine/'+dname+'/*.txt')
-    for f in fc:        
+    os.system('cp '+ws+'/cards/combine/'+dname+'/* '+wd+'/cards/combine/combine2pyhf/'+dname+'/.')
+    fc = glob.glob(wd+'/cards/combine/combine2pyhf/'+dname+'/*.txt')    
+    for f in fc:
         fname = f.split('/')[-1]
-        shapeloc(dname, f, combine2pyhf = True)
+        shapeloc(dname, f)
         print('combine -> pyhf: '+fname)
         os.system('python3 /HiggsAnalysis/CombinedLimit/test/datacardConvert.py '+f+' --out '+wd+'/cards/combine/combine2pyhf/'+dname+'/'+os.path.splitext(fname)[0])
         print('pyhf -> combine: '+fname)
