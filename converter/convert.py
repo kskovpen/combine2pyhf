54388baecba7ddfbb615141255eec40808d14cda
Kirill Skovpen <kskovpen@cern.ch>
2023-06-12 18:03:24 +0200

    update

--- converter/convert.py
+++ converter/convert.py
@@ -35,6 +35,10 @@ for d in dc:
         os.system('python3 /HiggsAnalysis/CombinedLimit/test/datacardConvert.py '+f+' --out '+wd+'/cards/combine/combine2pyhf/'+dname+'/'+os.path.splitext(fname)[0])
         print('pyhf -> combine: '+fname)
         os.system('python3 '+ws+'/converter/pyhf2combine.py --input '+wd+'/cards/combine/combine2pyhf/'+dname+'/'+fname.replace('.txt', '.json')+' --output '+wd+'/cards/combine/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0])
+        with open(wd+'/cards/combine/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0]+'.txt', 'r') as ff:
+            lines = ff.readlines()
+            for l in lines:
+                print(l)
     
 # pyhf cards
 dc = glob.glob(ws+'/cards/pyhf/*')
