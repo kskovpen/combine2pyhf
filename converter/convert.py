9bd70fc6f307254f1b9e7aec3d98d7e6aa7bb501
Kirill Skovpen <kskovpen@cern.ch>
2023-06-13 14:18:48 +0200

    update

--- converter/convert.py
+++ converter/convert.py
@@ -35,7 +35,7 @@ for d in dc:
         os.system('python3 /HiggsAnalysis/CombinedLimit/test/datacardConvert.py '+f+' --out '+wd+'/cards/combine/combine2pyhf/'+dname+'/'+os.path.splitext(fname)[0])
         print('pyhf -> combine: '+fname)
         os.system('python3 '+ws+'/converter/pyhf2combine.py --input '+wd+'/cards/combine/combine2pyhf/'+dname+'/'+fname.replace('.txt', '.json')+' --output '+wd+'/cards/combine/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0])
-        shapeloc(dname, f)
+        shapeloc(dname, wd+'/cards/combine/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0]+'.txt')
         with open(wd+'/cards/combine/pyhf2combine/'+dname+'/'+os.path.splitext(fname)[0]+'.txt', 'r') as ff:
             lines = ff.readlines()
             for l in lines:
