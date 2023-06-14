93431ba814899b2eea790f6529fa262f431da52d
Kirill Skovpen <kskovpen@cern.ch>
2023-06-14 17:51:59 +0200

    update

--- validation/combine.py
+++ validation/combine.py
@@ -79,17 +79,17 @@ if __name__ == '__main__':
             comblog.info('--> Run fits ('+dname+')')
             comblog.info('--> Prepare the workspace')
             execute(comblog, 'text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:defaultModel -o '+fname+'_model.root '+f)
-            for fit in fits:
+            for fit in fits.keys():
                 # get the best-fit snapshot
                 comblog.info('--> Perform the best fit ('+fit+')')
-                execute(comblog, 'combine -M MultiDimFit '+fit+'--saveWorkspace --expectSignal=1 -n BestFit '+opts+' '+fname+'_model.root')
+                execute(comblog, 'combine -M MultiDimFit '+fits[fit]+'--saveWorkspace --expectSignal=1 -n BestFit '+opts+' '+fname+'_model.root')
                 bf = postproc(comblog, getFitInfo('higgsCombineBestFit.MultiDimFit.mH120.root'))
                 comblog.info('bf='+str(bf['r'])+', nll='+str(bf['nll']))
                 # perform a NLL scan
                 rs = [0.68, 0.84, 1, 1.16, 1.32]
                 for r in rs:
                     comblog.info('--> Perform the fit for r='+str(r)+' ('+fit+')')
-                    execute(comblog, 'combine -M MultiDimFit -d higgsCombineBestFit.MultiDimFit.mH120.root -w w --snapshotName \"MultiDimFit\" -n Fit_r'+str(r)+' --setParameters r='+str(r)+' --freezeParameters r')
+                    execute(comblog, 'combine -M MultiDimFit '+fits[fit]+'-d higgsCombineBestFit.MultiDimFit.mH120.root -w w --snapshotName \"MultiDimFit\" -n Fit_r'+str(r)+' --setParameters r='+str(r)+' --freezeParameters r')
                     fres = postproc(comblog, getFitInfo('higgsCombineFit_r'+str(r)+'.MultiDimFit.mH120.root'))
                     comblog.info('rf='+str(fres['r'])+', deltaNLL='+str(fres['nll']))
 
