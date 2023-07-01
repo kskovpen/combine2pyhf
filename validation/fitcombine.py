bcf8e308cfb581859d377b61ccd051ae54259da3
Kirill Skovpen <kskovpen@cern.ch>
2023-07-01 15:16:43 +0200

    update

--- validation/fitcombine.py
+++ validation/fitcombine.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
                 comblog.info('--> Perform the best fit ('+fit+')')
                 utils.execute(comblog, 'combine -M MultiDimFit '+fits[fit]+'--saveWorkspace --saveNLL --expectSignal=1 -n BestFit '+opts+' '+fname+'_model.root')
                 bf = postproc(comblog, 'higgsCombineBestFit.MultiDimFit.mH120.root')
-                comblog.info('    bf='+str(bf['r'][0]))
+                comblog.info('    bf='+str(bf['r']))
                 comblog.info('--> Perform the scan ('+fit+')')
                 utils.execute(comblog, 'combine -M MultiDimFit '+fits[fit]+'-d higgsCombineBestFit.MultiDimFit.mH120.root --saveNLL -w w --snapshotName \"MultiDimFit\" -n Scan '+opts+' --algo grid --rMin '+str(options.min)+' --rMax '+str(options.max)+' --points '+str(options.npoints+1)+' --freezeParameters r --setParameters r=1 --alignEdges 1')
                 fres = postproc(comblog, 'higgsCombineScan.MultiDimFit.mH120.root', bf, ws+'/results', fit, fname.split('/')[-1])
