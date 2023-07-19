770d8dde0ad665f68b14504931f4dc415af4a92b
Kirill Skovpen <kskovpen@cern.ch>
2023-07-19 13:24:21 +0200

    update

--- validation/publish.py
+++ validation/publish.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     options = main()
 
-    desc = {'stat': 'MC statistical uncertainties', 'normsys': 'Normalization uncertainties', 'histosys': 'Shape uncertainties'}
+    desc = {'stat': 'MC statistical uncertainties', 'normsys': 'Normalization uncertainties', 'histosys': 'Shape uncertainties', 'normfactor': 'Unconstrained parameters', 'atlas': 'ATLAS cards', 'cms': 'CMS cards'}
     
     with open(options.output+'/README.md', 'w') as fr:
         intro = '# combine2pyhf\n\n An automated tool for a common validation of fit models using [combine](https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit) and [pyhf](https://github.com/scikit-hep/pyhf) packages.\n\n'
