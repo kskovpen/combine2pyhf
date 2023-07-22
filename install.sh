7377715eb69cc05bef7757cd432bab4ee4d31d25
Kirill Skovpen <kskovpen@cern.ch>
2023-07-22 23:34:31 +0200

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@
 root-config --version
 uname -a
 gcc --version
-cat /HiggsAnalysis/CombinedLimit/build/lib/python/HiggsAnalysis/CombinedLimit/DatacardConverter.py
 cd HiggsAnalysis/CombinedLimit
 /usr/bin/make LCG=1 -j 8
