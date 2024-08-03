b6c28765cadf4506faa0aa063264a349e868226a
Kirill Skovpen <kskovpen@cern.ch>
2024-08-03 14:29:30 +0200

    update

--- validation/test.sh
+++ validation/test.sh
@@ -10,8 +10,7 @@ pyenvon() {
   unset PYTHONPATH; unset PYTHONHOME
   /usr/bin/virtualenv --python=/usr/bin/python3 pyenv > /dev/null
   source pyenv/bin/activate
-  pip install --no-cache-dir pyhf iminuit deepdiff plotly kaleido pydash torch torchvision > /dev/null
-#  pip install pyhf iminuit deepdiff plotly kaleido pydash jax jaxlib torch torchvision > /dev/null
+  pip install pyhf iminuit deepdiff plotly kaleido pydash jax jaxlib torch torchvision > /dev/null
   source pyenv/bin/activate
 }
 
@@ -54,13 +53,15 @@ echo "Run combine fits on combine inputs .."
 python3 $WS/validation/fitcombine.py --combine
 check "$WS/logs/combine_fitcombine.log"
 echo "Done."
-echo "Run pyhf fits on combine inputs .."
-pyenvon; python3 $WS/validation/fitpyhf.py --combine; pyenvoff
-check "$WS/logs/combine_fitpyhf.log"
-echo "Done."
 echo "Run combine fits on pyhf inputs .."
 python3 $WS/validation/fitcombine.py
 check "$WS/logs/pyhf_fitcombine.log"
+echo "Done."
+echo "Clean combine."
+rm -rf /HiggsAnalysis
+echo "Run pyhf fits on combine inputs .."
+pyenvon; python3 $WS/validation/fitpyhf.py --combine; pyenvoff
+check "$WS/logs/combine_fitpyhf.log"
 echo "Done."
 echo "Run pyhf fits on pyhf inputs .."
 pyenvon; python3 $WS/validation/fitpyhf.py; pyenvoff
